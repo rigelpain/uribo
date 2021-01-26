68053a07131ee24ee52e2b15878242f7fb33fdd9
Kobayashi Haruaki <rigelpain@gmail.com>
2021-01-26 23:24:20 +0900

    [update] PC_ver1.0

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
 let swiper = new Swiper('.swiper-container', {
   loop: true,
   slidesPerView: 1,
+  breakpoints: {
+    426: {
+      slidesPerView: 2,
+      centeredSlides : true,
+    },
+    769: {
+      slidesPerView: 3,
+      centeredSlides : true,
+    }
+  },
   grabCursor: true,
-  initialSlide: 0,
-  spaceBetween: 100,
+  // initialSlide: 0,
+  // spaceBetween: 100,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
